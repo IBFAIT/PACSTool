51b6c1f5647f074f4a2d0b4ecc250ae8451495e8
IBFAIT <flavio.trolese@4quant.com>
2016-03-10 16:58:41 +0100

    Dummy test added

--- src/test/java/com/fourquant/riqae/pacs/DummyThirdPartyToolExecutorTest.java
+++ src/test/java/com/fourquant/riqae/pacs/DummyThirdPartyToolExecutorTest.java
@@ -1,22 +1,23 @@
 package com.fourquant.riqae.pacs;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
 
 public class DummyThirdPartyToolExecutorTest {
   @Test
-  public void testFoo() {
+  public void testFindDonatella() {
 
     final String[] dataFiles
-          = new String[]{"donatella.xml", "kate.xml", "ashlee.xml"};
+          = new String[]{"donatella.xml"};
 
     final DummyThirdPartyToolExecutor executor =
           new DummyThirdPartyToolExecutor(dataFiles);
 
     try {
-      final String[] result = executor.execute("find xy");
-      System.out.println("result = " + result);
+      final String[] result = executor.execute("findscu -...");
+      Assert.assertTrue(result[0].contains("Donatella"));
     } catch (IOException e) {
       e.printStackTrace();
     }
